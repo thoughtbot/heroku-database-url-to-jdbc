0562a9e5ada1d49a3edc2362b333a54f0d9ecb55
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-09 15:17:50 -0400

    Relax Clojure dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject heroku-database-url-to-jdbc "0.2.0"
+(defproject heroku-database-url-to-jdbc "0.2.1"
   :description "Converts Heroku-style DATABASE_URLs to JDBC-friendly strings or Korma maps"
   :url "https://github.com/thoughtbot/heroku-database-url-to-jdbc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
