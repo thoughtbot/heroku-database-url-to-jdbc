61bf57ae14ef1c8116742ab5de876fbd1dbd7802
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-09 13:26:01 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject heroku-database-url-to-jdbc "0.1.0-SNAPSHOT"
   :description "Converts Heroku-style DATABASE_URLs to JDBC-friendly maps or strings"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/thoughtbot/heroku-database-url-to-jdbc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
