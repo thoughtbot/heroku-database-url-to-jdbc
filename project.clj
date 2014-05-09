0ab436b7b09fa683960e7f5ce82e5071c95cf73e
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-09 14:51:31 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject heroku-database-url-to-jdbc "0.1.0"
+(defproject heroku-database-url-to-jdbc "0.1.1"
   :description "Converts Heroku-style DATABASE_URLs to JDBC-friendly strings or Korma maps"
   :url "https://github.com/thoughtbot/heroku-database-url-to-jdbc"
   :license {:name "Eclipse Public License"
