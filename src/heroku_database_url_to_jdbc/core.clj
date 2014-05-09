c561a541c9c0b8a742c81e95f12325e1723c48f9
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-09 13:54:59 -0400

    Create fn for returning raw JDBC string

--- src/heroku_database_url_to_jdbc/core.clj
+++ src/heroku_database_url_to_jdbc/core.clj
@@ -1,15 +1,31 @@
 (ns heroku-database-url-to-jdbc.core)
 
+(defn- create-uri [url] (java.net.URI. url))
+
 (defn- parse-user-and-password [db-uri]
   (clojure.string/split (.getUserInfo db-uri) #":"))
 
 (defn- subname [db-uri]
   (format "//%s:%s%s" (.getHost db-uri) (.getPort db-uri) (.getPath db-uri)))
 
-(defn heroku-database-url->jdbc-connection-map
-  "Converts Heroku's DATABASE_URL to a JDBC-friendly connection map"
+(defn heroku-database-url->jdbc-connection-string
+  "Converts Heroku's DATABASE_URL to a JDBC-friendly connection string"
   [heroku-database-url]
-  (let [db-uri (java.net.URI. heroku-database-url)
+  (let [db-uri (create-uri heroku-database-url)
+        [username password] (parse-user-and-password db-uri)]
+    (str "jdbc:postgresql://"
+         (.getHost db-uri)
+         ":"
+         (.getPort db-uri)
+         (.getPath db-uri)
+         "?user=" username
+         "&password=" password)))
+
+(defn heroku-database-url->korma-connection-map
+  "Converts Heroku's DATABASE_URL to a map that you can pass to Korma's
+  defdb fn"
+  [heroku-database-url]
+  (let [db-uri (create-uri heroku-database-url)
         [user password] (parse-user-and-password db-uri)]
     {:classname "org.postgresql.Driver"
      :subprotocol "postgresql"
--- test/heroku_database_url_to_jdbc/core_test.clj
+++ test/heroku_database_url_to_jdbc/core_test.clj
@@ -2,10 +2,16 @@
   (:require [clojure.test :refer :all]
             [heroku-database-url-to-jdbc.core :refer :all]))
 
-(deftest test-heroku-database-url->jdbc-connection-map
+(def heroku-database-url "postgres://user:pass@host:1234/path")
+
+(deftest test-heroku-database-url->korma-connection-map
   (is (= {:classname "org.postgresql.Driver"
           :subprotocol "postgresql"
           :user "user"
           :password "pass"
           :subname "//host:1234/path"}
-         (heroku-database-url->jdbc-connection-map "postgres://user:pass@host:1234/path"))))
+         (heroku-database-url->korma-connection-map heroku-database-url))))
+
+(deftest test-heroku-database-url->jdbc-connection-string
+  (is (= "jdbc:postgresql://host:1234/path?user=user&password=pass"
+         (heroku-database-url->jdbc-connection-string heroku-database-url))))
